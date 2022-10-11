90cf4b6bc1386a0ec33866e0a2ea4f5df1aef4df
AzizulHakim18 <hakimrony18@gamil.com>
2022-10-12 00:09:38 +0600

    navbar responsive

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     const routes = [
         { id: 1, name: "Home", path: '/home' },
         { id: 2, name: "Statistics", path: '/statistics' },
@@ -9,8 +11,14 @@ const Navbar = () => {
     ]
     return (
 
-        < nav >
-            <ul className='md:flex justify-end'>
+        < nav className='bg-purple-200 h-10' >
+
+            <div onClick={() => setOpen(!open)} className="h-10 w-10 text-black m-4 md:hidden" >
+                {
+                    open ? <XMarkIcon /> : <Bars3Icon />
+                }
+            </div>
+            <ul className={`bg-purple-200 md:flex w-full justify-end md:static absolute duration-500 ease-in ${open ? 'top-12' : 'top-[-120px]'}`}>
 
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
